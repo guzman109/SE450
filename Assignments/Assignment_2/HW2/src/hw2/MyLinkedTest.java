ae4deda1c89a26f61bd461946f052c397379eb42
Carlos <guzman.109@osu.edu>
2022-04-11 00:23:00 -0400

    Added hw2

---
+++ Assignments/Assignment_2/HW2/src/hw2/MyLinkedTest.java
@@ -0,0 +1,24 @@
+// package hw2;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// public class MyLinkedTest {
+    
+//     private final MyLinked b = new MyLinked();
+//     @Test
+//     // void testDelete () {
+//     //     b.add (1);
+//     //     assertEquals(1, b.size());
+//     //     b.delete (0);
+//     //     assertEquals(0, b.size());
+//     //     for (double i = 1; i < 13; i++) {
+//     //         b.add (i);
+//     //     }
+//     //     print ("bigger list", b);
+//     //     b.delete (0);
+//     //     print ("deleted at beginning", b);
+//     //     b.delete (10);
+//     //     print ("deleted at end", b);
+//     //     b.delete (4);
+//     //     print ("deleted in middle", b);
+//     // }
+// }
