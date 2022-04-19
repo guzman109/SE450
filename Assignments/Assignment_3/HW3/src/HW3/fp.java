71119780894197bc5b19dd556654f514e99a49c4
guzman109 <guzman.109@osu.edu>
2022-04-18 23:12:49 -0400

    Finished HW3

--- Assignments/Assignment_3/HW3/src/HW3/fp.java
+++ Assignments/Assignment_3/HW3/src/HW3/fp.java
@@ -5,95 +5,174 @@ import java.util.LinkedList;
 import java.util.function.Function;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
+
+
 import java.util.Comparator;
 
 public class fp {
 	
-// f takes U to V.. ie. f.apply(U): V
+	private static <U,V> void checkMapInvariant(Iterable<U> l, List<V> r) {
+		assert(l.iterator().hasNext() != r.isEmpty());
+	}
+	private static <U,V> boolean checkMapLoopInvariant(Iterable<U> l, Function<U,V> f, List<V> r, int pos) {
+		for (int i = 0; i <= pos; i++) {
+			boolean in_list = false;
+			for (U x: l) {
+				V m = f.apply(x);
+				if (m.hashCode() == r.get(i).hashCode())
+					in_list = true;
+			}
+			if (!in_list)
+				return false;
+		}
+
+		return true;
+	}
+	
+	/** Maps an iterable of generic type U to a List of generic type V
+	 * @invariant If l is empty then return value is empty.
+	 * @maintains Every element in r is mapped from an element in l.
+	 * @param l the iterable class to map to a list
+	 * @param f the mapping function from U to V
+	 * @return List<V> mapped List of type V
+	 */
+	// f takes U to V.. ie. f.apply(U): V
+		
+	public static <U,V> List<V> map(Iterable<U> l, Function<U,V> f) {
+		// walk through the U's
+		// use f at every stage f.apply
+		// construct list of V's
+		List<V> r = new LinkedList<>();
+		int pos = 0;
+		for (U x: l) {
+			r.add(f.apply(x));
+			assert checkMapLoopInvariant(l, f, r, pos);
+			pos++;
+		}
+
+		// assert
+		checkMapInvariant(l, r);
+		return r;
+	}
+
+	
+	/** 
+	 * 
+	 * @param e
+	 * @param Iterablel
+	 * @param f
+	 * @return V
+	 */
+	public static <U,V> V foldLeft(V e, Iterable<U>l, BiFunction<V,U,V> f){
+		// walk through the U's [u1,u2, ..,un]
+		//                       e
+		// use f at every stage v1= f.apply(e,u1)
+		//                         v2= f.apply(v1,u2)
+		//						    v3= f.apply(v2,u3)..
+		// return the last v
+		V v = e;
+		for (U u: l) {
+			v = f.apply(v, u);
+		}
+		return v;
+	}
 
 
 	
-public static <U,V> List<V> map(Iterable<U> l, Function<U,V> f) {
-	// walk through the U's
-	// use f at every stage f.apply
-	// construct list of V's
-	List<V> list = new LinkedList<>();
-	for (U x: l) {
-		list.add(f.apply(x));
+	/** 
+	 * @param v
+	 * @param Listl
+	 * @param BiFunctionf
+	 * @param i
+	 * @return V
+	 */
+	// similar to above
+	// but from the right
+	//     vn=  f(un,e)
+	//          vn-1 = f(un-1,vn)
+	// ..
+	// return the first v
+	private static <U,V> V foldRight_aux(V v, List<U>l, BiFunction<U,V,V>f, int i) {
+		if (i >= 0)
+			v = foldRight_aux(f.apply(l.get(i), v), l, f, --i);
+		return v;
 	}
+	
+	/** 
+	 * @param e
+	 * @param Listl
+	 * @param f
+	 * @return V
+	 */
+	public static <U,V> V foldRight(V e, List<U>l, BiFunction<U,V,V> f){
+		return foldRight_aux(e, l, f, l.size()-1);
+	}
+
+
+
+	
+	/** 
+	 * @param l
+	 * @param p
+	 * @return Iterable<U>
+	 */
+	public static <U> Iterable<U> filter(Iterable<U> l, Predicate<U> p){
+		LinkedList<U> list = new LinkedList<U>();
+		System.out.println(l);
+		for (U u:l)
+			if (p.test(u))
+				list.add(u);
 	return list;
-}
-
-public static <U,V> V foldLeft(V e, Iterable<U>l, BiFunction<V,U,V> f){
-	// walk through the U's [u1,u2, ..,un]
-	//                       e
-	// use f at every stage v1= f.apply(e,u1)
-	//                         v2= f.apply(v1,u2)
-	//						    v3= f.apply(v2,u3)..
-	// return the last v
-	V v = e;
-	for (U u: l) {
-		v = f.apply(v, u);
 	}
-	return v;
-}
-
-
-// similar to above
-// but from the right
-//     vn=  f(un,e)
-//          vn-1 = f(un-1,vn)
-// ..
-// return the first v
-private static <U,V> V foldRight_aux(V v, List<U>l, BiFunction<U,V,V>f, int i) {
-	if (i >= 0)
-		v = foldRight_aux(f.apply(l.get(i), v), l, f, --i);
-	return v;
-}
-public static <U,V> V foldRight(V e, List<U>l, BiFunction<U,V,V> f){
-	return foldRight_aux(e, l, f, l.size()-1);
-}
-
-
-
-public static <U> Iterable<U> filter(Iterable<U> l, Predicate<U> p){
-	LinkedList<U> list = new LinkedList<U>();
-	for (U u:l)
-		if (p.test(u))
-			list.addFirst(u);
-return list;
-}
-
-static <U> U minVal(Iterable<U> l, Comparator<U> c){
-	// write using fold.  No other loops or recursion permitted.
-	return foldLeft(l.iterator().next(), l, (U u1, U u2) -> {return c.compare(u1, u2) < 0 ? u1 : u2; }  );
-}
-
-// static <U extends Comparable<U>> int minPos(Iterable<U> l){
-// 	// write using fold.  No other loops or recursion permitted. 
-// 	l.iterator().next().compareTo(o)
-// 	return foldRight(0, l, (int x, int y)->{});
-// }
-
-	public static void main(String[] args) {
-
-		
-
-		
 
 	
-		
-		// (5) Use minVal to calculate the minimum of a List of 
-		//       Integers
-		
-        // (6) Use minVal to calculate the maximum of a List of 
-		//       Integers
-		
-		// (7)  Use minPos to calculate the position of the
-		// minimum in  a List of  Integers
+	/** 
+	 * @param l
+	 * @param c
+	 * @return U
+	 */
+	public static <U> U minVal(Iterable<U> l, Comparator<U> c){
+		// write using fold.  No other loops or recursion permitted.
+		return foldLeft(l.iterator().next(), l, (U u1, U u2) -> {return c.compare(u1, u2) < 0 ? u1 : u2; }  );
+	}
 
-		// (8)  Use minPos to calculate the position of the
-		// minimum in  a List of  String
+	private static class minPosAux<U extends Comparable<U>> implements BiFunction<U,U,U> {
+		private U min;
+		private Iterable<U> l;
+		private int min_pos = 0;
+		private boolean stop = false;
+
+		public minPosAux (Iterable<U> l) { 
+			this.l = l;
+			this.min = minVal(this.l, (U u1, U u2) -> { return u1.compareTo(u2); });
+		}
+
+		public int getMinPos() { return this.min_pos; }
+		@Override
+		public U apply(U t, U u) {
+			// TODO Auto-generated method stub
+			if (!this.stop) {
+				if ((this.min.compareTo(u) == 0)) 
+					this.stop = true;
+				else
+					this.min_pos++;
+			}
+
+			return u;
+		}
+		
+	}
+
+	
+	/** 
+	 * @param l
+	 * @return int
+	 */
+	public static <U extends Comparable<U>> int minPos(Iterable<U> l){
+		// write using fold.  No other loops or recursion permitted. 
+		minPosAux<U> f = new minPosAux<U>(l);
+		fp.foldLeft(null, l, f);
+		return f.getMinPos();
 	}
 
 }
--- Assignments/Assignment_3/HW3/src/Test/fpTest1.java
+++ Assignments/Assignment_3/HW3/src/Test/fpTest1.java
@@ -14,7 +14,7 @@ public class fpTest1 {
     
     List<Person> constructTestList() {
         List<Person> test_list = new LinkedList<Person>();
-        test_list.add(new Person(100000, "Mary"));
+        test_list.add(new Person(120000, "Mary"));
         test_list.add(new Person (75000,"Isla"));
         test_list.add(new Person(55000, "Sam"));
 
@@ -27,12 +27,12 @@ public class fpTest1 {
 
     private final List<Person> test_list = constructTestList();
 
-    // (1) Use map to implement the following behavior (described in Python).  i.e given a List<T> create a List<Integer> of the hashes of the objects.
-    // names = ['Mary', 'Isla', 'Sam']
-    // for i in range(len(names)):
-        // names[i] = hash(names[i])
     @Test
     void testMap() {
+        // (1) Use map to implement the following behavior (described in Python).  i.e given a List<T> create a List<Integer> of the hashes of the objects.
+        // names = ['Mary', 'Isla', 'Sam']
+        // for i in range(len(names)):
+            // names[i] = hash(names[i])
         List<Integer> result = fp.map(test_list, (Person p)->{return p.name().hashCode();});
         
         Integer[] expected = {"Mary".hashCode(), "Isla".hashCode(), "Sam".hashCode()};
@@ -40,31 +40,32 @@ public class fpTest1 {
         assertArrayEquals(expected, result.toArray());
     }
 
-    // (2) Use foldleft to calculate the sum of a list of integers.
-    // i.e write a method: int sum(List<Integer> l)
+
     @Test
     void testFoldLeft() {
+        // (2) Use foldleft to calculate the sum of a list of integers.
+        // i.e write a method: int sum(List<Integer> l)
         Integer result = fp.foldLeft(0, test_list, (Integer sum, Person p)->{ return p.getSalary() + sum; });
 
-        Integer expected = 100000+75000+55000;
+        Integer expected = 120000+75000+55000;
 
         assertEquals(expected, result);
     }
 
-    // (3) Use foldRight to concatenate a list of strings i.e write a method
-    // String s (List<String> l)
     @Test 
     void testFoldRight() {
+        // (3) Use foldRight to concatenate a list of strings i.e write a method
+        // String s (List<String> l)
         String result = fp.foldRight("", test_list, (Person p, String s)->{ return p.name().concat(s) ;});
         String expected = "MaryIslaSam";
         assertEquals(expected, result);
     }
 
-    // (4) consider an array of Persons. Use filter to 
-    // print the names of the Persons whose salary is
-    // greater than 100000
     @Test
     void testFilter() {
+        // (4) consider an array of Persons. Use filter to 
+        // print the names of the Persons whose salary is
+        // greater than 100000
         Iterable<Person> result = fp.filter(test_list, (Person p)-> { return p.getSalary() > 100000; });
         
         
@@ -82,12 +83,74 @@ public class fpTest1 {
             length_result++;
         }
 
-        print(names_expected);
-        print(names_result.toArray());
         assertArrayEquals(names_expected, names_result.toArray());
         assertArrayEquals(salaries_expected, salaries_result.toArray());
         assertEquals(length_expected, length_result);
+
+        Iterable<Person> result_empty = fp.filter(new LinkedList<Person>(), (Person p)-> { return p.getSalary() > 100000; });
+        LinkedList<Person> empty = new LinkedList<Person>();
+        
+        // Shouldn't run because list is empty.
+        for (Person p:result_empty)
+            empty.add(p);
+
+        assertArrayEquals((new LinkedList<>()).toArray(), empty.toArray());
     }
 
+    @Test
+    void testMinVal() {
+        // (5) Use minVal to calculate the minimum of a List of 
+		//       Integers
+		Person least_paid = fp.minVal(test_list, (Person p1, Person p2)->{ return Integer.compare(p1.getSalary(), p2.getSalary()); });
+        assertEquals("Sam", least_paid.name());
+        assertEquals(55000, least_paid.getSalary());
 
+        // (6) Use minVal to calculate the maximum of a List of 
+		//       Integers
+        Person most_paid = fp.minVal(test_list, (Person p1, Person p2)->{ return Integer.compare(p1.getSalary(), p2.getSalary())*(-1); });
+        assertEquals("Mary", most_paid.name());
+        assertEquals(120000, most_paid.getSalary());
+
+        LinkedList<Integer> int_list = new LinkedList<Integer>();
+        
+        for (int i=0; i<5000; i++)
+            int_list.addFirst(i+234);
+        
+        int minVal = fp.minVal(int_list, (Integer x, Integer y)->{ return Integer.compare(x, y); });
+        assertEquals(234, minVal);
+
+        int maxVal = fp.minVal(int_list, (Integer x, Integer y)->{ return Integer.compare(x,y)*(-1); });
+        assertEquals(5233, maxVal);
+    }
+
+    @Test
+    void testMinPos() {
+        // (7)  Use minPos to calculate the position of the
+		// minimum in  a List of  Integers
+        int min_pos_Int = fp.minPos( fp.map(test_list, (Person p)->{ return p.getSalary(); }) );
+        assertEquals(2, min_pos_Int);
+
+		// (8)  Use minPos to calculate the position of the
+		// minimum in  a List of  String
+        int min_pos_Str = fp.minPos( fp.map(test_list, (Person p)->{ return p.name(); }) );
+        assertTrue(1==min_pos_Str);
+
+        LinkedList<Integer> int_list = new LinkedList<Integer>();
+        int_list.add(0);
+        int_list.add(1);
+        int_list.add(-1);
+        int_list.add(2);
+        int_list.add(3);
+        int int_result = fp.minPos(int_list);
+        assertEquals(2, int_result);
+
+        LinkedList<String> str_list = new LinkedList<String>();
+        str_list.add("A");
+        str_list.add("B");
+        str_list.add("Z");
+        str_list.add("a");
+        str_list.add("A");
+        int str_result = fp.minPos(str_list);
+        assertTrue(0==str_result);
+    }
 }
