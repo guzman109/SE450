d6c0f76c07a13176a0d7ffc3b7b7cfec536eb37f
guzman109 <guzman.109@osu.edu>
2022-05-09 23:40:08 -0400

    finished

--- Assignments/Assignment_6/HW6/src/shop/main/Main.java
+++ Assignments/Assignment_6/HW6/src/shop/main/Main.java
@@ -8,11 +8,11 @@ public class Main {
   private Main() {}
   public static void main(String[] args) {
     UI ui;
-    // if (Math.random() <= 0.5) {
-    //   ui = new shop.ui.TextUI();
-    // } else {
+    if (Math.random() <= 0.5) {
+      ui = new shop.ui.TextUI();
+    } else {
       ui = new shop.ui.PopupUI();
-    // }
+    }
     Control control = new Control(Data.newInventory(), ui);
     control.run();
   }
